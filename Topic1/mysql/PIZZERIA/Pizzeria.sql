fc37e945b5212e1ed0884808718b57f9d5b15bcb
Bartpbcn <bartpauwels@me.com>
2024-04-30 01:00:30 +0200

    change address table of position

--- Topic1/mysql/PIZZERIA/Pizzeria.sql
+++ Topic1/mysql/PIZZERIA/Pizzeria.sql
@@ -1,12 +1,24 @@
 USE Pizzeria;
 
+CREATE TABLE Addresses(
+    AddressID INT AUTO_INCREMENT PRIMARY KEY,
+    Street VARCHAR(255),
+    Streetnumber INT,
+    Floor INT,
+    Door INT,
+    PostalCode VARCHAR(10),
+    City VARCHAR(100),
+    Province VARCHAR(100),
+    Country VARCHAR(100)
+);
+
 CREATE TABLE Customers (
     CustomerID INT AUTO_INCREMENT PRIMARY KEY,
     FirstName VARCHAR(100),
     LastName VARCHAR(100),
-    Telephone VARCHAR(15)
+    Telephone VARCHAR(15),
     AddressID INT,
-    FOREIGN KEY(AddressID) REFERENCES addresses(AddressID)
+    FOREIGN KEY(AddressID) REFERENCES Addresses(AddressID)
 );
 CREATE TABLE Orders (
     OrderID INT AUTO_INCREMENT PRIMARY KEY,
@@ -61,15 +73,4 @@ CREATE TABLE OrderDeliveries (
     PRIMARY KEY (OrderID, EmployeeID)
 );
 
-CREATE TABLE Adresses(
-    AddressID INT AUTO_INCREMENT PRIMARY KEY,
-    Street VARCHAR(255),
-    Streetnumber INT(10),
-    Floor INT(10),
-    Door INT(10),
-    PostalCode VARCHAR(10),
-    City VARCHAR(100),
-    Province VARCHAR(100),
-    Country VARCHAR(100)
-);
 
